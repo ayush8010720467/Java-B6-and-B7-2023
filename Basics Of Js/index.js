508fa7a0c47a73872e464ba81ad55de755c88296
Ayush Katiyar <ayushkatiyar096@gmail.com>
2023-02-21 00:16:31 +0530

    Add files via upload

---
+++ Basics Of Js/index.js
@@ -0,0 +1,65 @@
+console.log('Hi from external file');
+
+// TODO: What are DataTypes?
+// The Type of data that we are dealing with is reffered as data types
+// Premitive DataType / Non primetive Data Type
+// toDO: what are Premetive Datatypes?
+// Datatypes that are Predefined.
+// TODO: what are non Premitive Datatypes?
+// They are the customised datatype
+
+// TODO: In JS what are the Premitive DT available to us?
+// number
+// string
+// boolean
+// undefined
+// null
+// symbol
+
+// TODO: in JS what are the no Premitive DTs available to us?
+// function
+// object
+// arrays
+
+// TODO: Datatypes
+
+// number
+//   ---> Infinity
+//  ---> -Infinity
+// ---> NaN (not a number)
+//  NaN == NaN  ---> Always be false
+// "Ayush" -> NaN
+// "Katiyar" -> NaN
+// isNaN(variable) --> would return true if it is NaN and false if Not.
+
+// TODO: What is variable Declariation?
+// "Programers dont declare War the declare variables."
+// we can declare variables in js via "let", "const" and "var" keyword.
+
+var a;
+let b;
+const c;
+// TODO : Why is there the need of 3 keywords for declaring a variable?
+  // Scoping
+  // TODO: what is scoping
+  // The area of visibility of the variable is reffered as scoping.
+  // TODO: is there any type of scoping available?
+  // there are 2 types of scoping
+  // 1. functional Scope ->
+    // Means even if the variable is defined at the last lin of the function then also it
+    //  would be accessible to the first line of the function without giving any errors.
+  // 2. Blocked Scope ->
+    // TODO: What Block is?
+      // if the peice of code is written within certain curly braces then it is a block of code.
+      // For Eg.
+        // {
+        //  ...
+        //  }
+        // in this eg the block of code is within the curly braces is ...
+    // Means if the variable is declared in  a block then it is accessible within that block and only after its declaration.
+  // if any variable is defined with var the it is a functional scoped variable
+  // if any variable is defined with let or const then it is blocked scope variable
+// the difference b/w const and let is that let variable can be redefiend but with const that is not the case.
+
+
+
